6fde36c9bd7f8f101ed25292a5522f48a1764686
Марина Бузина <vorona.2003.bu@yandex.ru>
2022-04-28 21:41:07 +0300

    Переопределена модель User

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,1 +1,24 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+ROLE_CHOICES = (
+    'user',
+    'moderator',
+    'admin',
+)
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        'Почта',
+        unique=True
+    )
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+    role = models.CharField(
+        'Роль',
+        choices=ROLE_CHOICES,
+        default='user'
+    )
