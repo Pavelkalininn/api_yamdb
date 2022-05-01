c96373f209d890eedca502868bc58e0d037b2e95
Марина Бузина <vorona.2003.bu@yandex.ru>
2022-05-01 13:21:18 +0300

    Добавлено разрешение

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,17 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Permission, Group
+from django.contrib.auth.models import AbstractUser  # , Permission, Group
 
 ROLE_CHOICES = (
     'user',
     'moderator',
     'admin',
 )
-Group.objects.bulk_create([
-    Group('user'),
-    Group('moderator'),
-    Group('admin'),
-])
-# Добавить разрешения в группы
 
 
 class User(AbstractUser):
@@ -44,24 +38,6 @@ class User(AbstractUser):
         if self.role == 'moderator':
             return True
         return False
-
-    def get_user_permissions(self, obj):
-        if self.is_superuser:
-            return Permission.objects.all()
-        # elif self.is_admin:
-        return Permission.objects.filter(group__user=self.role)
-        # elif self.is_moderator:
-        #     ...
-        # elif self.is_anonymous:
-        #     ...
-        # else:
-        #     ...
-        # return super().get_user_permissions(obj)
-
-    class Meta:
-        permissions = [
-            ('can_choose_user_role', 'Can choose the role of user'),
-        ]
 
 
 class Genre(models.Model):
