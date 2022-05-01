6eaa258190d379058a7fe4563c650adb64d94501
Марина Бузина <vorona.2003.bu@yandex.ru>
2022-05-01 12:26:44 +0300

    Добавлен файл разрешений и переопределена модель юзер

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Permission
 
 ROLE_CHOICES = (
     'user',
@@ -22,3 +22,32 @@ class User(AbstractUser):
         choices=ROLE_CHOICES,
         default='user'
     )
+
+    @property
+    def is_admin(self):
+        if self.role == 'admin':
+            return True
+        return False
+
+    @property
+    def is_staff(self):
+        return self.is_admin or self.is_superuser
+
+    @property
+    def is_moderator(self):
+        if self.role == 'moderator':
+            return True
+        return False
+
+    def get_user_permissions(self, obj):
+        if self.is_superuser:
+            return Permission.objects.all()
+        # elif self.is_admin:
+        return Permission.objects.filter(group__user=self.role)
+        # elif self.is_moderator:
+        #     ...
+        # elif self.is_anonymous:
+        #     ...
+        # else:
+        #     ...
+        # return super().get_user_permissions(obj)
